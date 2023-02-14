a9fcb3f5c1d5e7e153821ec4e5f767d0569dd7fe
antarm <antarm@outlook.com>
2023-02-14 22:53:20 +0800

    add header

---
+++ data_structure/link_list.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   link_list.py
+@Time    :   2023/02/14 22:53:06
+@Author  :   ufy
+@Contact :   antarm@outlook.com
+@Version :   v1.0
+@Desc    :   None
+'''
+
+# here put the import lib
