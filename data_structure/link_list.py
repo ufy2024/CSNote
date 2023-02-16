cbabaff3a93e70838ba0946dd40431bd98a1a34a
ufy <antarm@outlook.com>
2023-02-16 20:59:36 +0800

    完成doubleLinkedList

--- data_structure/link_list.py
+++ data_structure/link_list.py
@@ -11,6 +11,8 @@
 
 from typing import Iterable
 
+from requests import delete
+
 
 #-----------------------------------------------------------------------------------------
 # single linked list
@@ -65,7 +67,7 @@ class LinkedList:
                 cur.next = node
             self.count += 1
 
-    def delete(self, i) -> ListNode:
+    def delete(self, i):
         '''删除linkedList的第i个位置的元素,并返回该节点的值.'''
         assert i >= 0 and i < self.count, f"i的取值范围为[0,{len(self)}),且i为整数."
         cur = self.head
@@ -109,7 +111,7 @@ class LinkedList:
         while cur:
             datas.append(str(cur.element))
             cur = cur.next
-        out = '[' + '->'.join(datas) + ']'
+        out = '[' + ' -> '.join(datas) + ']'
         return out
 
     def __repr__(self) -> str:
@@ -128,7 +130,7 @@ class LinkedList:
 #-----------------------------------------------------------------------------------------
 # doublle linked list
 #-----------------------------------------------------------------------------------------
-class ListNode2Link:
+class ListNode2Direction:
 
     def __init__(self, element=None, prior=None, next=None) -> None:
         self.element = element
@@ -136,12 +138,73 @@ class ListNode2Link:
         self.next = next
 
 
-class DoubleLinkedList:
+class DoubleLinkedList(LinkedList):
 
     def __init__(self) -> None:
         self.head = None
         self.count = 0
 
     def create(self, datas: Iterable):
+        cur = None
         for e in datas:
-            node = ListNode2Link(element=e)
+            node = ListNode2Direction(element=e)
+            if self.head == None:
+                self.head = node
+                cur = self.head
+            else:
+                cur.next = node
+                node.prior = cur
+            self.count += 1
+
+    def add_first(self, e):
+        node = ListNode2Direction(element=e)
+        if self.head == None:
+            self.head = None
+        else:
+            node.next = self.head
+            self.head.prior = node
+            self.head = node
+        self.count += 1
+
+    def insert(self, i, e):
+        """在DoubleLinkedList的第i个位置插入节点(e)"""
+        assert i >= 0 and i <= self.count, f"i的取值范围为[0,{len(self)}],且i为整数."
+        if i == 0:
+            self.add_first(e=e)
+        else:
+            cur = self.head
+            while i - 1 > 0:
+                cur = cur.next
+                i -= 1
+            node = ListNode2Direction(element=e)
+            node.prior = cur
+            node.next = cur.next
+            cur.next = node
+        self.count += 1
+
+    def delete(self, i):
+        '''删除linkedList的第i个位置的元素,并返回该节点的值.'''
+        assert i >= 0 and i < self.count, f"i的取值范围为[0,{len(self)}),且i为整数."
+        cur = self.head
+        if i == 0:
+            self.head = cur.next
+            self.head.prior = None
+            out = cur
+        else:
+            while i - 1 > 0:
+                cur = cur.next
+                i -= 1
+            out = cur.next
+            cur.next = cur.next.next
+            cur.next.prior = cur
+        self.count -= 1
+        return out.element
+
+    def __str__(self) -> str:
+        cur = self.head
+        datas = []
+        while cur:
+            datas.append(str(cur.element))
+            cur = cur.next
+        out = '[' + ' <-> '.join(datas) + ']'
+        return out
