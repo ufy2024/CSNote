3464abcccf668ba2351bd58664985e887206119e
ufy <antarm@outlook.com>
2023-02-21 16:26:53 +0800

    create

---
+++ data_structure/__init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Time    :   2023/02/21 16:23:10
+@Author  :   ufy
+@Contact :   antarm@outlook.com
+@Version :   v1.0
+@Desc    :   None
+'''
+# here put the import lib
